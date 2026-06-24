a5764cffd733556438494c40f8f6309cd667db2a
Your Name <you@example.com>
2026-06-24 19:27:00 +0300

    Fix CSS and Vercel deployment

---
+++ Desktop/Ebby Digital Solutions/js/supabase.js
@@ -0,0 +1,12 @@
+const SUPABASE_URL=https://gycdnuhoxxgxiwardjms.supabase.co
+"YOUR_SUPABASE_URL=sb_publishable_bTAjnYpH2N9qJ6tzDbPDHQ_JAZcjCIP
+;
+
+const SUPABASE_KEY =
+"YOUR_SUPABASE_ANON_KEY";
+
+const supabaseClient =
+supabase.createClient(
+SUPABASE_URL,
+SUPABASE_KEY
+);
